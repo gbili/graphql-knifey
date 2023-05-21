724d8e8c4a2a6db58069dba1f01b09d3a424715e
Guillermo Pages <mail@guillermo.at>
2023-05-22 00:54:12 +0200

    fix: Trouble loading JWT_KEY_PUBLIC in dependants

--- src/loaders/env.ts
+++ src/loaders/env.ts
@@ -2,28 +2,30 @@ import 'dotenv/config';
 import { LoadDictElement } from 'di-why/build/src/DiContainer';
 import { UnknownEnv, areDBKeysInEnv, envHasKeyGen, getTypedKey } from 'swiss-army-knifey';
 
-const env: UnknownEnv = {};
-
-const pubKeyInEnv = 'JWT_KEY_PUBLIC';
-env[pubKeyInEnv] = getTypedKey(process.env, pubKeyInEnv);
-const hasKey = envHasKeyGen(process.env);
-
-if (!hasKey('JWT_ALGORITHM')) {
-  env['JWT_ALGORITHM'] = 'RS256';
-}
-
-if (!areDBKeysInEnv(process.env)) {
-  throw new Error('Missing some Db Env Vars');
-}
-
 const loadDictElement: LoadDictElement<object> = {
-  instance: {
-    ...{
-      LOGGER_LOG: '0',
-      LOGGER_DEBUG: '1',
-    },
-    ...process.env,
-    ...env,
+  factory: () => {
+    const env: UnknownEnv = {};
+
+    const pubKeyInEnv = 'JWT_KEY_PUBLIC';
+    env[pubKeyInEnv] = getTypedKey(process.env, pubKeyInEnv);
+    const hasKey = envHasKeyGen(process.env);
+
+    if (!hasKey('JWT_ALGORITHM')) {
+      env['JWT_ALGORITHM'] = 'RS256';
+    }
+
+    if (!areDBKeysInEnv(process.env)) {
+      throw new Error('Missing some Db Env Vars');
+    }
+
+    return {
+      ...{
+        LOGGER_LOG: '0',
+        LOGGER_DEBUG: '1',
+      },
+      ...process.env,
+      ...env,
+    };
   },
 };
 
