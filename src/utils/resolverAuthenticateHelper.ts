fa627f5deb653d700dd7fd3c52ba90a312eddc98
Guillermo Pages <mail@guillermo.at>
2024-10-30 15:46:40 +0100

    fix: type errors

--- src/utils/resolverAuthenticateHelper.ts
+++ src/utils/resolverAuthenticateHelper.ts
@@ -1,9 +1,17 @@
 import { AuthenticateRequestAndPlugUserInInputParam } from "../generalTypes";
-
-export function authenticateRequestAndPlugUserInInput<T extends {}>({ tokenAuthService, token }: AuthenticateRequestAndPlugUserInInputParam, input?: T) {
+type UserAnyProps = {
+  [k: string]: any;
+  UUID: string;
+};
+export function authenticateRequestAndPlugUserInInput<T extends {}>({ tokenAuthService, token }: AuthenticateRequestAndPlugUserInInputParam, input?: T): { user: UserAnyProps } & T {
   const { token: notNeeded, ...user } = tokenAuthService.authenticateTokenStrategy({ token, tokenConfig: {} });
+
   if (typeof user !== 'object' || user.UUID === undefined) {
     throw new Error('Unable to authenticate from token.');
   }
-  return { ...(input ? input : {}), user };
+
+  return {
+    ...(input ? input : {}) as T,
+    user
+  };
 }
