06293c0b5c5632859ea8fd085dc43da97b9954bb
Guillermo Pages <mail@guillermo.at>
2025-09-23 09:06:09 +0200

    feat: update di-why

--- src/utils/gkAppConfigMapListAdd.ts
+++ src/utils/gkAppConfigMapListAdd.ts
@@ -1,11 +1,7 @@
-import { createAppConfigMapListAdder, AppConfigNamespace } from 'di-why/build/src/index';
+import { AppConfigNamespace } from 'di-why/build/src/index';
 import { prefixHandle } from './prefixHandle';
 
 export const gkAppConfigMapNamespace: AppConfigNamespace = {
   namespace: prefixHandle('gkAppConfigMap'),
   priority: 50,
 };
-
-export const gkAppConfigMapListAdd = createAppConfigMapListAdder([gkAppConfigMapNamespace]);
-
-export default gkAppConfigMapListAdd;
